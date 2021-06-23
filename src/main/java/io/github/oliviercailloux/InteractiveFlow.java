749e5d3c34c54bc7db40d552c76c4264cef3f9be
samba914 <sambadiarra2000@gmail.com>
2021-06-23 20:35:27 +0200

    creation branch

--- src/main/java/io/github/oliviercailloux/InteractiveFlow.java
+++ src/main/java/io/github/oliviercailloux/InteractiveFlow.java
@@ -5,27 +5,44 @@ import java.util.List;
 import com.azure.identity.InteractiveBrowserCredential;
 import com.azure.identity.InteractiveBrowserCredentialBuilder;
 import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
 import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
-import com.microsoft.graph.models.User;
+import com.microsoft.graph.models.DriveSearchParameterSet;
+import com.microsoft.graph.requests.DriveSearchCollectionPage;
 import com.microsoft.graph.requests.GraphServiceClient;
 
 import okhttp3.Request;
 
 public class InteractiveFlow {
-	private final static String CLIENT_ID = "4b5d4470-571b-4602-816e-87cc364008e7";
+	private final static String CLIENT_ID = "afd352ef-e48a-4244-9340-95bfc83ef33c";
 	private final static List<String> SCOPES = ImmutableList.of("User.Read", "Files.ReadWrite");
 
 	public static void main(String[] args) throws Exception {
+
 		final InteractiveBrowserCredential interactiveBrowserCredential = new InteractiveBrowserCredentialBuilder()
-				.clientId(CLIENT_ID).redirectUrl("http://localhost/").build();
+				.tenantId("common").clientId(CLIENT_ID).redirectUrl("http://localhost/").build();
 		final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(SCOPES,
 				interactiveBrowserCredential);
 
 		final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
 				.authenticationProvider(tokenCredentialAuthProvider).buildClient();
 
-		final User me = graphClient.me().buildRequest().get();
-		System.out.println(me.mail);
+		/*
+		 * DriveItem driveItem = new DriveItem(); driveItem.name = "New Folder"; Folder
+		 * folder = new Folder(); driveItem.folder = folder;
+		 * driveItem.additionalDataManager().put("@microsoft.graph.conflictBehavior",
+		 * new JsonPrimitive("rename"));
+		 */
 
+		DriveSearchCollectionPage search = graphClient.me().drive()
+				.search(DriveSearchParameterSet.newBuilder().withQ("Book1.xlsx").build()).buildRequest().get();
+		Gson gson = new Gson();
+		String json = gson.toJson(search);
+		System.out.println(json);
+
+		/*
+		 * final User me = graphClient.me().buildRequest().get();
+		 * System.out.println(me.displayName); System.out.println("done");
+		 */
 	}
 }
